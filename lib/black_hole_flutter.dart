c8419e8a6a08d2ef3d9d48c490f30cb4e2b0cde4
Jonas Wanke <contact@jonas-wanke.com>
2020-03-12 18:47:11 +0100

    feat: add ChipGroup

--- lib/black_hole_flutter.dart
+++ lib/black_hole_flutter.dart
@@ -1,6 +1,7 @@
 // TODO(JonasWanke): library comment
 library black_hole_flutter;
 
+export 'src/chip_group.dart';
 export 'src/color.dart';
 export 'src/color_material.dart';
 export 'src/context.dart';
---
+++ lib/src/chip_group.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'context.dart';
+
+/// Wraps multiple [Chip]s and can optionally show a [title] above these.
+class ChipGroup extends StatelessWidget {
+  const ChipGroup({
+    Key key,
+    this.title,
+    @required this.children,
+  })  : assert(children != null),
+        super(key: key);
+
+  /// An optional [Widget] that is displayed above the [children] if there are
+  /// any.
+  final Widget title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child = Wrap(
+      crossAxisAlignment: WrapCrossAlignment.center,
+      spacing: 8,
+      children: children,
+    );
+
+    if (title != null && children.isNotEmpty) {
+      child = Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          DefaultTextStyle(
+            style: context.textTheme.overline,
+            child: title,
+          ),
+          child,
+        ],
+      );
+    }
+
+    return child;
+  }
+}
