10ed4b83b1f367271df960c7b9bddb10cec0f11c
Jonas Wanke <contact@jonas-wanke.com>
2020-03-19 19:00:52 +0100

    feat(example): add FancyBottomSheet example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:black_hole_flutter/black_hole_flutter.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,8 +7,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'black_hole_flutter example',
-      home: SizedBox(),
+      title: '🛠 black_hole_flutter example',
+      home: Scaffold(
+        appBar: AppBar(title: Text('🛠 black_hole_flutter example')),
+        body: ListView(children: <Widget>[
+          BottomSheetExample(),
+        ]),
+      ),
+    );
+  }
+}
+
+class BottomSheetExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Section(
+      title: 'FancyBottomSheet',
+      children: <Widget>[
+        FancyRaisedButton(
+          onPressed: () {
+            context.showFancyModalBottomSheet(
+              builder: (_) => Padding(
+                padding: EdgeInsets.symmetric(vertical: 128, horizontal: 16),
+                child: Text("I'm fancy!"),
+              ),
+            );
+          },
+          child: Text('Open FancyBottomSheet'),
+        ),
+      ],
+    );
+  }
+}
+
+class Section extends StatelessWidget {
+  const Section({Key key, @required this.title, @required this.children})
+      : assert(title != null),
+        assert(children != null),
+        super(key: key);
+
+  final String title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(title, style: context.textTheme.subhead),
+          SizedBox(height: 8),
+          Wrap(
+            spacing: 16,
+            runSpacing: 16,
+            children: children,
+          ),
+        ],
+      ),
     );
   }
 }
