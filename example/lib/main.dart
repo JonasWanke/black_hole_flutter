cd166dc750487fc0a501bd368bedb08e925a37a8
Jonas Wanke <contact@jonas-wanke.com>
2020-03-19 19:32:04 +0100

    feat(example): add ChipGroup example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         body: ListView(children: <Widget>[
           BottomSheetExample(),
           ButtonsExample(),
+          ChipGroupExample(),
         ]),
       ),
     );
@@ -37,6 +38,40 @@ class BottomSheetExample extends StatelessWidget {
             );
           },
           child: Text('Open FancyBottomSheet'),
+        ),
+      ],
+    );
+  }
+}
+
+class ChipGroupExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Section(
+      title: 'ChipGroup',
+      children: <Widget>[
+        ChipGroup(
+          children: <Widget>[
+            Chip(label: Text('Lorem')),
+            Chip(label: Text('ipsum')),
+            Chip(label: Text('dolor')),
+            Chip(label: Text('sit')),
+            Chip(label: Text('amet')),
+            Chip(label: Text('consectetur')),
+            Chip(label: Text('adipiscing')),
+            Chip(label: Text('elit')),
+            Chip(label: Text('sed')),
+            Chip(label: Text('do')),
+            Chip(label: Text('eiusmod')),
+            Chip(label: Text('tempor')),
+            Chip(label: Text('incididunt')),
+            Chip(label: Text('ut')),
+            Chip(label: Text('labore')),
+            Chip(label: Text('et')),
+            Chip(label: Text('dolore')),
+            Chip(label: Text('magna')),
+            Chip(label: Text('aliqua')),
+          ],
         ),
       ],
     );
