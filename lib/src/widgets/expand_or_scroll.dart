1f6f9d884977b1568aa4b6b91e9325d48672bd31
Jonas Wanke <contact@jonas-wanke.com>
2023-01-24 16:29:39 +0100

    Add expandOrScroll.key, .scrollDirection

--- lib/src/widgets/expand_or_scroll.dart
+++ lib/src/widgets/expand_or_scroll.dart
@@ -2,36 +2,48 @@ import 'package:flutter/widgets.dart';
 
 class ExpandOrScroll extends StatelessWidget {
   const ExpandOrScroll({
-    this.padding = EdgeInsets.zero,
+    super.key,
     this.wrapInSafeArea = true,
+    this.padding = EdgeInsets.zero,
+    this.scrollDirection = Axis.vertical,
     this.scrollController,
     required this.child,
   });
 
-  final EdgeInsetsGeometry padding;
   final bool wrapInSafeArea;
+  final EdgeInsetsGeometry padding;
+  final Axis scrollDirection;
   final ScrollController? scrollController;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     var child = this.child;
+
     if (wrapInSafeArea) child = SafeArea(child: child);
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return SingleChildScrollView(
-          controller: scrollController,
-          padding: padding,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minWidth: constraints.maxWidth,
-              minHeight: constraints.maxHeight - padding.vertical,
-            ),
-            child: IntrinsicHeight(child: child),
+    return LayoutBuilder(builder: (context, constraints) {
+      return SingleChildScrollView(
+        padding: padding,
+        scrollDirection: scrollDirection,
+        controller: scrollController,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: constraints.maxWidth,
+            minHeight: constraints.maxHeight - padding.vertical,
           ),
-        );
-      },
-    );
+          child: _intrinsicAlongAxis(scrollDirection, child),
+        ),
+      );
+    });
+  }
+}
+
+Widget _intrinsicAlongAxis(Axis axis, Widget child) {
+  switch (axis) {
+    case Axis.horizontal:
+      return IntrinsicWidth(child: child);
+    case Axis.vertical:
+      return IntrinsicHeight(child: child);
   }
 }
