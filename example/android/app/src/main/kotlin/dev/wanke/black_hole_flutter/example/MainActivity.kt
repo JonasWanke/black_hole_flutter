c248104198af6d124a280782f8c9adac2c8bc992
Jonas Wanke <contact@jonas-wanke.com>
2026-01-02 11:50:29 +0100

    Regenerate Android platform folder

--- example/android/app/src/main/kotlin/dev/wanke/black_hole_flutter/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/dev/wanke/black_hole_flutter/example/MainActivity.kt
@@ -2,5 +2,4 @@ package dev.wanke.black_hole_flutter.example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
