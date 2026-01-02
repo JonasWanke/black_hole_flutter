4ed8b21fa797bfd657fe65d35e193cf663daa842
Jonas Wanke <contact@jonas-wanke.com>
2026-01-02 14:54:15 +0100

    Make circle.diameter optional

--- lib/src/widgets/circle.dart
+++ lib/src/widgets/circle.dart
@@ -6,28 +6,30 @@ import '../context.dart';
 class Circle extends StatelessWidget {
   const Circle({
     super.key,
-    required this.diameter,
+    this.diameter,
     this.backgroundColor,
     this.padding = EdgeInsets.zero,
     this.child,
   });
 
-  final double diameter;
+  final double? diameter;
   final Color? backgroundColor;
   final EdgeInsetsGeometry padding;
   final Widget? child;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: diameter,
-      height: diameter,
+    final child = Container(
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: backgroundColor ?? context.theme.colorScheme.primary,
       ),
       padding: padding,
-      child: child,
+      child: this.child,
     );
+
+    return diameter == null
+        ? AspectRatio(aspectRatio: 1, child: child)
+        : SizedBox(width: diameter, height: diameter, child: child);
   }
 }
